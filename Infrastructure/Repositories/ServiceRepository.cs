0269f211ca969f3f1a2de07a9685b2f77a11b680
kareem <abdelkareemsalah58@gmail.com>
2023-04-18 17:53:15 +0200

    changed search action

--- Infrastructure/Repositories/ServiceRepository.cs
+++ Infrastructure/Repositories/ServiceRepository.cs
@@ -25,7 +25,7 @@ namespace Infrastructure.Repositories
             i.Reviews.Average(r => r.Rating) > rating &&
             i.Location.CityName == city).OrderByDescending(i=>i.Reviews.Average(i=>i.Rating))
             .ThenBy(i => (LevenshteinDistance.Calculate(searchString, i.Name)))
-            .Skip(skip).Take(take).Select(i => ServiceSearchDTO.FromService(i, searchString , i.Name, i.Reviews.Average(i => i.Rating)));
+            .Skip(skip).Take(take).Select(i => ServiceSearchDTO.FromService(i, searchString , i.Name, i.Reviews.Average(i => i.Rating),i.Reviews.FirstOrDefault().Content));
             return result;
         }
     }
